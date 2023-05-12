05a15b0e65b6055d395991d617de2afbf8803494
koodoyoon <kooodoyoon@gmail.com>
2023-05-12 13:45:34 +0900

    first commit hello winterview

---
+++ src/main/java/com/example/winterview/WinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.winterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/winterview/WinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
