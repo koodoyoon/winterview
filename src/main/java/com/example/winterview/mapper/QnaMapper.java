41d55e8b00b2da47db1cfc5dffebe65ffb0e1d9f
koodoyoon <kooodoyoon@gmail.com>
2023-06-11 17:49:43 +0900

    modify mapper service

--- src/main/java/com/example/winterview/mapper/QnaMapper.java
+++ src/main/java/com/example/winterview/mapper/QnaMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface QnaMapper {
     int insert(QnaDto qna);
+
     QnaDto selectRandomQuestion(int userIdx);
 
     int updateAnswer(QnaDto qna);
--- src/main/java/com/example/winterview/service/QnaService.java
+++ src/main/java/com/example/winterview/service/QnaService.java
@@ -4,7 +4,9 @@ import com.example.winterview.dto.QnaDto;
 
 public interface QnaService {
     int register(QnaDto qna);
+
     QnaDto getRandomQuestion(int userIdx);
+
     QnaDto selectOne(int questionIdx);
 
     int addAnswer(QnaDto qnaDto);
